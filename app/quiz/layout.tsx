633e889e254633bde56fe7ae7087e8af98026c8d
Mats Lading <mjlading@ThinkPadMats>
2024-01-17 01:19:19 +0100

    first commit

---
+++ app/quiz/layout.tsx
@@ -0,0 +1,12 @@
+export default function QuizLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section>
+      <h1>Quiz Layout</h1>
+      {children}
+    </section>
+  );
+}
